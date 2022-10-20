d7af3c03c51be55ea3278a5df57d9a8d1a5f93c3
Gavin Shaughnessy <gavinshaughnessy@gmail.com>
2022-10-20 19:32:23 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+pub enum PieceType {
+    pawn,
+    knight,
+    bishop,
+    rook,
+    queen,
+    king
+}
+
+pub enum PieceColour {
+
+}
+
+
+
+fn main() {
+    println!("Hello, world!");
+}
+
+
+
+struct Piece {
+    
+}
+
+struct Board {
+
+}
