3435c1c0674513d7daaa05e953b2eaa9cabf5090
Gavin Shaughnessy <gavinshaughnessy@gmail.com>
2024-08-17 22:16:08 +0100

    cargo fmt

--- src/mailbox.rs
+++ src/mailbox.rs
@@ -21,8 +21,6 @@ const MAILBOX64: [i32; 64] = [
 #[inline(always)]
 pub fn next_mailbox_number(i: usize, j: i32) -> i32 {
     // MAILBOX64[i] + j should always be a valid index
-    unsafe {
-        *MAILBOX.get_unchecked((*MAILBOX64.get_unchecked(i) + j) as usize)
-    }
+    unsafe { *MAILBOX.get_unchecked((*MAILBOX64.get_unchecked(i) + j) as usize) }
     // MAILBOX[(MAILBOX64[i] + j) as usize]
 }
