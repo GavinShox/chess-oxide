2daa82c9b8d55ac8c197f97a7572c52d946d88ec
Gavin Shaughnessy <gavinshaughnessy@gmail.com>
2024-08-21 16:46:29 +0100

    gui: changed to fluent theme, added left padding to move history

--- build.rs
+++ build.rs
@@ -1,4 +1,4 @@
 fn main() {
-    let config = slint_build::CompilerConfiguration::new().with_style("cosmic".into());
+    let config = slint_build::CompilerConfiguration::new().with_style("fluent".into());
     slint_build::compile_with_config("ui/board_ui.slint", config).unwrap();
 }
