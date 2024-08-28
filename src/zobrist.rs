a5f11a71e7b49d3599abe40cc0ed3708dbab0b90
Gavin Shaughnessy <gavinshaughnessy@gmail.com>
2024-08-28 23:39:05 +0100

    WIP adding boardstate info to zobrist hashes

--- src/zobrist.rs
+++ src/zobrist.rs
@@ -1,8 +1,29 @@
 use rand::Rng;
 
-use crate::movegen::*;
+use static_init::dynamic;
 
+use crate::{movegen::*, Position};
+
+// static table, to ensure all positions that are equal have the same hashes for the duration of the program
+#[dynamic]
+static ZOBRIST_HASH_TABLE: ZobristHashTable = ZobristHashTable::new();
+// using 64 bit hashes
 pub type PositionHash = u64;
+
+// zobrist hash of full Position, used to initialise the board
+pub fn pos_hash(pos: &Position) -> PositionHash {
+    ZOBRIST_HASH_TABLE.full_position_hash(pos)
+}
+
+// increment the zobrist hash of a Position, can be used when moves are made instead of calling pos_hash on the whole position every move
+pub fn pos_next_hash(current_hash: PositionHash, mv: &Move) -> PositionHash {
+    todo!()
+}
+
+// add BoardState information into a zobrist Position hash
+pub fn board_state_hash(current_hash: PositionHash, occurrences: u8, halfmove_count: u32) -> PositionHash {
+    todo!()
+}
 
 struct ZobristHashTable {
     pos_table: [[PositionHash; 12]; 64],
@@ -16,7 +37,7 @@ struct ZobristHashTable {
     occurrences: [PositionHash; 3]
 }
 impl ZobristHashTable {
-    pub fn new() -> Self {
+    fn new() -> Self {
         let mut rng = rand::thread_rng();
         let mut pos_table: [[PositionHash; 12]; 64] = [[0; 12]; 64];
         for i in 0..64 {
@@ -54,19 +75,72 @@ impl ZobristHashTable {
         }
     }
 
-    pub fn next(&self, current_hash: PositionHash, mv: &Move) -> PositionHash {
+    pub fn next_hash(&self, current_hash: PositionHash, mv: &Move) -> PositionHash {
         todo!() //https://www.chessprogramming.org/Zobrist_Hashing
     }
 
-    #[inline]
-    pub fn get_piece_hash(&self, piece: &Piece, square_idx: usize) -> PositionHash {
-        unsafe {
-            *self.pos_table.get_unchecked(square_idx).get_unchecked(Self::get_piece_idx(piece))
+    pub fn full_position_hash(&self, pos: &Position) -> PositionHash {
+        let mut hash = 0;
+        for (i, s) in pos.pos64.iter().enumerate() {
+            match s {
+                Square::Piece(p) => {
+                    hash ^= self.get_piece_hash(p, i);
+                }
+                Square::Empty => {
+                    continue;
+                }
+            }
         }
-        //self.pos_table[square_idx][Self::get_piece_idx(piece)]
+        if pos.movegen_flags.white_castle_long {
+            hash ^= self.white_castle_long;
+        }
+        if pos.movegen_flags.black_castle_long {
+            hash ^= self.black_castle_long;
+        }
+        if pos.movegen_flags.white_castle_short {
+            hash ^= self.white_castle_short;
+        }
+        if pos.movegen_flags.black_castle_short {
+            hash ^= self.black_castle_short;
+        }
+        if pos.movegen_flags.en_passant.is_some() {
+            hash ^= self.en_passant_table[pos.movegen_flags.en_passant.unwrap() % 8];
+        }
+        if pos.side == PieceColour::Black {
+            hash ^= self.black_to_move;
+        }
+
+        hash
     }
 
-    #[inline]
+    fn board_state_hash(&self, current_hash: PositionHash, occurrences: u8, halfmove_count: u32) -> PositionHash {
+        current_hash ^ self.get_occurrences_hash(occurrences) ^ self.halfmove_count[halfmove_count as usize]
+    }
+
+    #[inline(always)]
+    fn get_halfmove_count_hash(&self, halfmove_count: u32) -> PositionHash {
+        self.halfmove_count[halfmove_count as usize]
+    }
+
+    #[inline(always)]
+    fn get_occurrences_hash(&self, occurrences: u8) -> PositionHash {
+        match occurrences {
+            1 => self.occurrences[0],
+            2 => self.occurrences[1],
+            3 => self.occurrences[2],
+            _ => unreachable!()
+        }
+    }
+
+    #[inline(always)]
+    fn get_piece_hash(&self, piece: &Piece, square_idx: usize) -> PositionHash {
+        // unsafe {
+        //     *self.pos_table.get_unchecked(square_idx).get_unchecked(Self::get_piece_idx(piece))
+        // }
+        self.pos_table[square_idx][Self::get_piece_idx(piece)]
+    }
+
+    #[inline(always)]
     fn get_piece_idx(piece: &Piece) -> usize {
         match piece.pcolour {
             PieceColour::White => match piece.ptype {
