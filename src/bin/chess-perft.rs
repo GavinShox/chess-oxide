5bdc03cbb412b048b19265d310b5047e802be669
Gavin Shaughnessy <gavinshaughnessy@gmail.com>
2024-08-31 17:00:36 +0100

    chess-perft now gives average time after multiple iterations, hardcoded 10 iters for now

--- src/bin/chess-perft.rs
+++ src/bin/chess-perft.rs
@@ -1,20 +1,44 @@
+use std::time::{Duration, Instant};
+
 use env_logger::{Builder, Target};
 
 fn main() {
+    // initialise logger
     let mut builder = Builder::from_default_env();
     builder.target(Target::Stdout);
     builder.init();
 
     let pos = chess::Position::new_starting();
     let board = chess::board::Board::new();
-    //let board = chess::board::Board::from_fen("r1b2rk1/1pp3pp/p2bpq2/3p4/8/2P3P1/PP1BQPBP/3R1RK1 b - - 1 15").unwrap();
-    //let mut pos = Position::from_fen_partial_impl("rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8").unwrap().0;
-    //let mut pos = Position::new_position_from_fen("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1");
-    //r1b2rk1/1pp3pp/p2bpq2/3p4/8/2P3P1/PP1BQPBP/3R1RK b - - 1 15
 
-    chess::perft(&pos, 5);
-    chess::engine_perft(&board.current_state, 7);
+    let pos_perft_iterations = 10;
+    let mut total_pos_perft_time = Duration::new(0, 0);
+    for i in 0..pos_perft_iterations {
+        let start = Instant::now();
+        chess::perft(&pos, 5);
+        let duration = start.elapsed();
+        total_pos_perft_time += duration;
+        println!(
+            "Time elapsed in position perft iteration {}: {:?}",
+            i + 1,
+            duration
+        );
+    }
+    println!("Total time elapsed in position perft: {:?} (after {} iterations)\nAverage time per iteration: {:?}", total_pos_perft_time, pos_perft_iterations, total_pos_perft_time / pos_perft_iterations);
 
-    // let board = Board::from_fen("rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8").unwrap();
-    // assert_eq!(board.to_fen(), "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8".to_owned());
+    let engine_iterations = 10;
+    let mut total_engine_time = Duration::new(0, 0);
+    for i in 0..engine_iterations {
+        let start = Instant::now();
+        //chess::perft(&pos, 5);
+        chess::engine_perft(&board.current_state, 7);
+        let duration = start.elapsed();
+        total_engine_time += duration;
+        println!(
+            "Time elapsed in engine perft iteration {}: {:?}",
+            i + 1,
+            duration
+        );
+    }
+    println!("Total time elapsed in engine perft: {:?} (after {} iterations)\nAverage time per iteration: {:?}", total_engine_time, engine_iterations, total_engine_time / engine_iterations);
 }
