0907e1bda0d5683f70fb04e087b6438274ccb09b
Gavin Shaughnessy <gavinshaughnessy@gmail.com>
2024-10-13 00:56:50 +0100

    slint gui now an optional feature to avoid bloating library with dependancies

--- build.rs
+++ build.rs
@@ -1,4 +1,10 @@
+#[cfg(feature = "gui")]
 fn main() {
     let config = slint_build::CompilerConfiguration::new().with_style("fluent".into());
     slint_build::compile_with_config("ui/main.slint", config).unwrap();
+}
+
+#[cfg(not(feature = "gui"))]
+fn main() {
+    // no build script needed
 }
